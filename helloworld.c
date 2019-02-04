d2f9d8aaa6a7f687c71540fe039cf76a5d51690e
cybercrypter2810 <megarockystarman@gmail.com>
2019-02-04 13:59:57 -0800

    helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+// hello.c
+// compile gcc hello.c
+// run ./a.out
+#include <stdio.h>
+int main(){
+	printf("********* Hello World *******\n");
+	printf("*            from           *\n");
+	printf("*  GGG B     B P     P MMM  *\n");
+	printf("*  G    B   B   P   P    M  *\n");
+	printf("*  GGG    B       P    MMM  *\n");
+	printf("*****************************\n");
+	return 0;	
+}
