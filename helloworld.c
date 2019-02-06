33a14fd89c75177739bfc4814a6b149d4c06fd90
cybercrypter2810 <megarockystarman@gmail.com>
2019-02-06 13:38:20 -0800

    helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -3,16 +3,18 @@
 // run ./a.out
 #include <stdio.h>
 int main(){
-	printf("********* Hello World                                                                    *******\n");
-	printf("*            from                                                                         *\n");
-	printf("*   GGGGGGGG                            MMM                    MMM                       *\n");
-	printf("*   GGGGGGGG       BBBBBB    PPPPPP     MMM MMM            MMM MMM                                       *\n");
-	printf("*   GG             BB    BB  PP    PP   MMM  MMM          MMM  MMM                                        *\n");
-	printf("*   GG   GGGGGG    BBBBBB    PPPPPP     MMM   MMM        MMM   MMM                                         *\n");
-	printf("*   GG   GGGGGG    BBBBBB    PP         MMM    MMM      MMM    MMM                                      *\n");
-	printf("*   GG     GG      BB    BB  PP         MMM      MMM   MMM     MMM                               *\n");
-	printf("*   GGGGGGGGG      BBBBBB    PP         MMM       MMM MMM      MMM                           *\n");
-	printf("*   GGGGGGGGG                           MMM         MMM        MMM                        *\n");
-	printf("***************************                                           **\n");
+	printf("********************* Hello World ***********************************\n");
+	printf("*                        from                                       *\n");
+	printf("*   GGGGGGGGG      BBBBBB    PPPPPP     MM                     MM   *\n");
+	printf("*   GGGGGGGGG      BBBBBB    PPPPPP     MM MM               MM MM   *\n");
+	printf("*   GG             BB    BB  PP    PP   MM  MM             MM  MM   *\n");
+	printf("*   GG             BB    BB  PP    PP   MM   MM           MM   MM   *\n");
+	printf("*   GG   GGGGGG    BBBBBB    PPPPPP     MM    MM         MM    MM   *\n");
+	printf("*   GG   GGGGGG    BBBBBB    PPPPPP     MM     MM       MM     MM   *\n");
+	printf("*   GG     GG      BB    BB  PP         MM      MM     MM      MM   *\n");
+	printf("*   GG     GG      BB    BB  PP         MM       MM   MM       MM   *\n");
+	printf("*   GGGGGGGGG      BBBBBB    PP         MM        MM MM        MM   *\n");
+	printf("*   GGGGGGGGG      BBBBBB    PP         MM          MM         MM   *\n");
+	printf("*********************************************************************\n");
 	return 0;	
 }
